6d3984a3048ede46d18282d9241208cfbb212269
GuriDonghan <snuffy@naver.com>
2022-03-11 01:14:46 +0900

    Initial Commit

---
+++ GuessedTheFlag2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessedTheFlag2
+//
+//  Created by USER on 2022/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessedTheFlag2/GuessedTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessedTheFlag2App.swift
+//  GuessedTheFlag2
+//
+//  Created by USER on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct GuessedTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
